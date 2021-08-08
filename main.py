9c688942ba4a07c0585741868fa16f2ec2fc8b7f
fujilix <mappi20031126@gmail.com>
2021-08-08 22:20:35 +0900

    Fixes frame rate issues and Improved compression.
    The frame rate has been changed so that it will always be 30fps when dropping the frame rate. The previous specification was to first drop the frame rate to 1/4, and then to 1/2 if the file size did not reach 8MB or less, but this made no sense and was a waste of time.
    
    Added a message window.
    It was hard to tell when the process was finished, so I added it. This is a change for the exe file.
    
    Optimized to work with Pyinstaller.
    Pyinstaller was giving an error when using the --no-console option. This was caused by subprocess, and has been fixed.

--- main.py
+++ main.py
@@ -3,123 +3,167 @@ import math
 import os
 import re
 import subprocess
+# import sys
 import tkinter.filedialog
 import tkinter.messagebox
+# import codecs
+
 
 # reset
+def tempremove():
+    if os.path.exists('temp.mp4'):
+        os.remove('temp.mp4')
+
+
+# For PyInstaller
+def subprocess_args(include_stdout=True):
+    if hasattr(subprocess, 'STARTUPINFO'):
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        env = os.environ
+    else:
+        si = None
+        env = None
+    if include_stdout:
+        ret = {'stdout': subprocess.PIPE}
+    else:
+        ret = {}
+    ret.update({'stdin': subprocess.PIPE,
+                'stderr': subprocess.PIPE,
+                'startupinfo': si,
+                'env': env})
+    return ret
+
+
+# if sys.stdout.encoding != 'UTF-8':
+#     sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
+# if sys.stderr.encoding != 'UTF-8':
+#     sys.stderr = codecs.getwriter('utf-8')(sys.stderr.buffer, 'strict')
+
+# Path
+maindir = os.path.dirname(os.path.realpath(__file__))
+ffpath = maindir + '\\ffmpeg.exe'
+print(ffpath)
+ls_file_name = os.listdir()
+print(ls_file_name)
 if os.path.exists('output.mp4'):
     os.remove('output.mp4')
 
-if os.path.exists('temp.mp4'):
-    os.remove('temp.mp4')
-path = os.getcwd()
-ffpath = path + r'\ffmpeg.exe'
+tempremove()
 filesize = 0
 suc = 0
 cycle = 4
+retry = 0
+
 # tkinter
-name = 'U8MB'
-root = tkinter.Tk()
-root.withdraw()
-fTyp = [("", "*")]
-iDir = os.path.abspath(os.path.dirname(__file__))
-sel = tkinter.messagebox.askyesno(name, 'Use NVENC codec?\nIf you have an NVIDIA graphics board, you can expect '
-                                        'higher speed.')
-if sel:
-    codec = 'h264_nvenc'
-    prefix = 'cq'
-    quality = 20
-    nvenc = "-b:v 0"
-else:
-    codec = 'h264'
-    prefix = 'crf'
-    quality = 18
-    nvenc = ""
-tkinter.messagebox.showinfo(name, 'Select the source video.')
-file = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
-rawfile = file
-root, ext = os.path.splitext(file)
-print(ext)
-
-# extension
-if ext == ".mp4":
-    # filesize calculation
-    filesize = math.ceil(os.path.getsize(file) / 1000000)
-    print(filesize, "MB")
-    incommand = f'"{ffpath}" -i "{file}'
-    rawinfo = subprocess.run(incommand, capture_output=False, text=True, input="y", stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-    info = rawinfo.stderr
-    countmp = info.find('fps')
-    temps = (info[countmp - 6:countmp])
-    if 's' in temps:
-        tempfps = re.sub(r"\D", "", temps)
-    else:
-        tempfps = temps
-    resultfps = float(tempfps)
-
-    while suc == 0:
-        if os.path.exists('temp.mp4'):
-            os.remove('temp.mp4')
-        if 55 > resultfps:
-            fpswitch = 0
-            fpsprefix = ""
+while suc == 0:
+    if retry == 0:
+        name = '8mb-compressor'
+        root = tkinter.Tk()
+        root.withdraw()
+        fTyp = [("", "*")]
+        iDir = os.path.abspath(os.path.dirname(__file__))
+        sel = tkinter.messagebox.askyesno(name,
+                                          'Use NVENC codec?\nIf you have an NVIDIA graphics board, you can expect '
+                                          'higher speed.')
+        if sel:
+            codec = 'h264_nvenc'
+            prefix = 'cq'
+            quality = 20
+            nvenc = "-b:v 0"
         else:
-            # Drop fps
-            fpswitch = 1
-            cycle_str = cycle.__str__()
-            fpsprefix = "-vf decimate=cycle=" + cycle_str
-            command = f'"{ffpath}" -i "{rawfile}" -vcodec {codec} {nvenc} {fpsprefix} "temp.mp4'
-            print(command)
-            print("Drop fps")
-            proc = subprocess.run(command, capture_output=False, text=True, input="y", stdout=subprocess.PIPE,
-                                  stderr=subprocess.PIPE)
-            print(proc.stderr)
-            file = "temp.mp4"
-            filesize = math.ceil(os.path.getsize('temp.mp4') / 1000000)
-            print(filesize)
-            if filesize >= 100:
-                quality = 50
+            codec = 'h264'
+            prefix = 'crf'
+            quality = 18
+            nvenc = ""
+        tkinter.messagebox.showinfo(name, 'Select the source video.')
+
+    file = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir, title='Select the source video.')
+    rawfile = file
+    root, ext = os.path.splitext(file)
+    print(ext)
+
+    if ext == ".mp4":
+
+        # TODO: tkinter gui
+        # GET Frame rate
+        filesize = math.ceil(os.path.getsize(file) / 1000000)
+        print(filesize, "MB")
+        incommand = f'"{ffpath}" -y -i "{file}'
+        rawinfo = subprocess.run(incommand, **subprocess_args(True), text=True)
+        info = rawinfo.stderr
+        print(info)
+        countmp = info.find('fps')
+        temps = (info[countmp - 6:countmp])
+        if 's' in temps:
+            tempfps = re.sub(r"\D", "", temps)
+        else:
+            tempfps = temps
+        print(tempfps)
+        resultfps = float(tempfps)
+
+        while suc == 0:
+            tempremove()
+            if 55 > resultfps:
+                fpswitch = 0
             else:
-                quality = 30
-        if filesize >= 250 and fpswitch == 0:
-            quality = 35
-        # ffmpeg
-        while filesize > 8:
-
-            command = f'"{ffpath}" -i "{file}" -vcodec {codec} {nvenc} -{prefix} "{quality}" "output.mp4"'
-            print(command)
-            proc = subprocess.run(command, capture_output=False, text=True, input="y", stdout=subprocess.PIPE,
-                                  stderr=subprocess.PIPE)
-
-            # log output
-            print(proc.stderr)
-
-            if os.path.exists('output.mp4'):
-                filesize = math.ceil(os.path.getsize('output.mp4') / 1000000)
-                raw = os.path.getsize('output.mp4')
-                print(filesize, "MB", raw)
-                if codec == "h264_nvenc":
-                    if raw <= 9000000 and quality < 51 or quality == 50:
-                        quality = quality + 1
-                    else:
-                        quality = quality + 10
-                        if quality > 51 and fpswitch == 1 and filesize > 8 and cycle == 4:
-                            cycle = 2
-                            break
-                        elif quality > 51 and filesize > 8 and cycle == 2:
-                            print("last phase failed.")
-                            suc = 1
-                            break
+                # Drop fps
+                fpswitch = 1
+                cycle_str = cycle.__str__()
+                command = f'"{ffpath}" -y -i "{rawfile}" -vcodec {codec} {nvenc} -vf decimate=cycle=2 temp.mp4'
+                print(command)
+                print("Drop fps")
+                proc = subprocess.run(command, **subprocess_args(True), text=True)
+                print(proc.stderr)
+                file = "temp.mp4"
+                filesize = math.ceil(os.path.getsize('temp.mp4') / 1000000)
+                print(filesize)
+                if filesize >= 100:
+                    quality = 50
                 else:
-                    quality = quality + 1
+                    quality = 30
+            if filesize >= 250 and fpswitch == 0:
+                quality = 35
+            while filesize > 8:
+
+                command = f'"{ffpath}" -y -i "{file}" -vcodec {codec} {nvenc} -{prefix} "{quality}" "output.mp4"'
+                print(command)
+                proc = subprocess.run(command, **subprocess_args(True))
+                print(proc.stderr)
+
+                if os.path.exists('output.mp4'):
+                    filesize = math.ceil(os.path.getsize('output.mp4') / 1000000)
+                    raw = os.path.getsize('output.mp4')
+                    print(filesize, "MB", raw)
+                    if codec == "h264_nvenc":
+                        if raw <= 9000000 and quality < 51 or quality == 50:
+                            quality = quality + 1
+                        else:
+                            quality = quality + 10
+                            if quality > 51 and filesize > 8:
+                                print("last phase failed.")
+                                tkinter.messagebox.showwarning(name, 'Compression failed.')
+                                suc = 1
+                                break
+                    else:
+                        quality = quality + 1
+
+                else:
+                    print("FFMPEG ERROR")
+                    tkinter.messagebox.showwarning(name, 'output.mp4 was not found.')
+                    break
 
             else:
-                print("FFMPEG ERROR")
-                break
+                print("COMPRESSED")
+                tkinter.messagebox.showinfo(name, 'Compression successful.')
+                suc = 1
+                tempremove()
 
-        else:
-            print("COMPRESSED")
-            suc = 1
-else:
-    tkinter.messagebox.showinfo('name', 'Only supports mp4 format.')
+    elif ext == "":
+        print("Cancelled")
+        break
+
+    else:
+        tkinter.messagebox.showinfo(name, 'Only supports mp4 format.')
+        retry = 1
+        continue
